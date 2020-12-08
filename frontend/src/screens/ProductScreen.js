69acfd5ecba8ea6268ee011de4a6a1f85a1c9fa1
AbdushukurRasulov <abdushukur9717@mail.ru>
2020-12-08 22:08:13 +0500

    setup first backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import products from '../assets/products';
 import Rating from '../components/Rating';
 
 const ProductScreen = ({ match }) => {
-  const product = products.find(product => product._id === match.params .id);
-  console.log(product);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${ match.params.id }`);
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, []);
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">Go Back</Link>  
