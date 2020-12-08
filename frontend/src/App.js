368eeef29eefcedd367b853d641de0ed27b96594
AbdushukurRasulov <abdushukur9717@mail.ru>
2020-12-08 17:59:41 +0500

    Product list, Product screen, Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
